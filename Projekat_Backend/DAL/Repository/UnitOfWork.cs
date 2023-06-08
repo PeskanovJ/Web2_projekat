fa07e6a5484a9d5f9ed73c1d540d404bc6989e8a
PeskanovJ <jovan.peskanov@gmail.com>
2023-06-08 18:16:06 +0200

    Added UnitOfWork with repository pattern

---
+++ Projekat_Backend/DAL/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Context;
+using DAL.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+
+            User = new UserRepository(_db);
+
+        }
+        public IUserRepository User { get; set; }
+
+        public void Save()
+        {
+
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Projekat_Backend/DAL/Repository/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Context;
+using DAL.Model;
+using DAL.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    internal class UserRepository : Repository<User>, IUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(User obj)
+        {
+            _db.Users.Update(obj);
+        }
+    }
+}
