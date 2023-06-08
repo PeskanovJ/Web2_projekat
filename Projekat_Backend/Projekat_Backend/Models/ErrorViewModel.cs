149de70d06f4a5e36d135b65395067a11f7dcb25
PeskanovJ <jovan.peskanov@gmail.com>
2023-06-08 17:11:55 +0200

    Changed to WebApi project

--- Projekat_Backend/Projekat_Backend/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Projekat_Backend.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
