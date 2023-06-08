224dfa2315bfae1f832923d2dfaa33b9f410f9e5
PeskanovJ <jovan.peskanov@gmail.com>
2023-06-08 18:15:34 +0200

    Added user model and first migration

---
+++ Projekat_Backend/DAL/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public byte[] Password { get; set; }
+        public byte[] Salt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public SD.Roles Role { get; set; }
+        public Guid ActivationGuid { get; set; } //guid koji cemo koristiti za aktivaciju naloga
+        public Guid PasswordGuid { get; set; } //guid koji cemo koristiti za reset passworda
+        public string ProfileUrl { get; set; } //putanja do profilne slike
+    }
+}
--- Projekat_Backend/Projekat_Backend/Program.cs
+++ Projekat_Backend/Projekat_Backend/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using DAL.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,6 +24,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+
 
 app.MapControllers();
 
